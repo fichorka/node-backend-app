c202e15625fe6a6932cf5356b9d4e64a4820107c
fichorka <fichorka@gmail.com>
2020-09-06 06:36:55 +0200

    Implement delete

--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -1,4 +1,5 @@
 import express from "express";
+import session from "express-session";
 
 const router = express.Router();
 
@@ -10,22 +11,59 @@ router.get("/:username", async (req, res) => {
   }, 3000);
 
   const { username } = req.params;
-  const user = await req.app.locals.db.users
-    .findOne({ username })
-    .catch((err) => {
+  if (username && username === req.app.locals.user.username) {
+    const user = await req.app.locals.db.users
+      .findOne({ username })
+      .catch((err) => {
+        throw err;
+      });
+
+    if (user) {
+      res.render("layout", {
+        page: "profilePartial",
+        pageProps: {
+          username: req.app.locals.user.username,
+          accountDate: "Sep 20",
+        },
+      });
+    } else {
+      res.status(404).render("layout", {
+        page: "messagePartial",
+        pageProps: {
+          message: "User doesn't exist.",
+        },
+      });
+    }
+  } else {
+    res.status(401).render("layout", {
+      page: "messagePartial",
+      pageProps: {
+        message: "Unauthorized.",
+      },
+    });
+  }
+});
+
+router.post("/:username", async (req, res) => {
+  const { username } = req.params;
+  if (username && username === req.app.locals.user.username) {
+    await req.app.locals.db.users.deleteOne({ username }).catch((err) => {
       throw err;
     });
-
-  if (user) {
+    req.session.destroy();
     res.render("layout", {
-      page: "profilePartial",
+      page: "messagePartial",
       pageProps: {
-        username: user.username,
-        accountDate: "Sep 20",
+        message: "Profile Successfully deleted.",
       },
     });
   } else {
-    res.status(404).send("User doesn't exist");
+    res.status(401).render("layout", {
+      page: "messagePartial",
+      pageProps: {
+        message: "Unauthorized.",
+      },
+    });
   }
 });
 
