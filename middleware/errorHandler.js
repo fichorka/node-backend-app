084dba9ab2751184d23962a516a537b1e0305b03
fichorka <fichorka@gmail.com>
2020-09-07 15:19:47 +0200

    Fix error handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,9 @@
-export default async function errorHandler(error) {
-  res.render("layout", {
+export default async function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500).render("layout", {
     page: "messagePartial",
-    pageProperties: { message: error.message },
+    pageProperties: { message: err.message },
   });
 }
